6bf762c7c177de9cda9a89b03ff8b63ff9e7753b
AustinPotts <pottsevolvements@gmail.com>
2019-11-24 12:23:17 -0500

    Project Configured

---
+++ WatchReactions/WatchReactions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WatchReactions
+//
+//  Created by Austin Potts on 11/24/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
