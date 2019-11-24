e704244ec7e41d99d1137a2d47e5ccacce02d0c9
AustinPotts <pottsevolvements@gmail.com>
2019-11-24 12:55:01 -0500

    Configured Sound Actions

--- WatchReactions/WatchReactions WatchKit Extension/InterfaceController.swift
+++ WatchReactions/WatchReactions WatchKit Extension/InterfaceController.swift
@@ -8,9 +8,12 @@
 
 import WatchKit
 import Foundation
+import AVFoundation
 
 
-class InterfaceController: WKInterfaceController {
+class InterfaceController: WKInterfaceController, SoundPlaying {
+    
+    var audioPlayer: AVAudioPlayer?
 
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
@@ -30,18 +33,22 @@ class InterfaceController: WKInterfaceController {
     
 
     @IBAction func playSound1() {
+        playSound(named: "alien-spaceship_daniel_simion")
     }
     
     
     @IBAction func playSound2() {
+        playSound(named: "alien-spaceship_daniel_simion")
     }
     
     
     @IBAction func playSound3() {
+        playSound(named: "alien-spaceship_daniel_simion")
     }
     
     
     @IBAction func playSound4() {
+        playSound(named: "alien-spaceship_daniel_simion")
     }
     
 }
